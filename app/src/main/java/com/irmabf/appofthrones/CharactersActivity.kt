277f943fe6c1c7bedf8e58568be150b339af89c0
Irma <irblancofdz@gmail.com>
2018-09-05 22:39:37 +0200

    Add CharactersActivity, refactor Main Activity. Now we launch the app in Characters Activity Screen

--- app/src/main/java/com/irmabf/appofthrones/CharactersActivity.kt
+++ app/src/main/java/com/irmabf/appofthrones/CharactersActivity.kt
@@ -1,4 +1,20 @@
 package com.irmabf.appofthrones
 
-class CharactersActivity {
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Button
+
+class CharactersActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_characters)
+
+        val button: Button = findViewById(R.id.button_character)
+
+        button.setOnClickListener {
+            Log.d("Characters Activity", "Click is working")
+        }
+    }
 }
