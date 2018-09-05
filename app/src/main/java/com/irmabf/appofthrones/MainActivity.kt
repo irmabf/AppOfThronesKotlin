381d1a6e3c77e0709edf59e50e41d9912f8329d7
Irma <irblancofdz@gmail.com>
2018-09-05 22:05:21 +0200

    Refactor button OnClickListener to a kotlin lambda

--- app/src/main/java/com/irmabf/appofthrones/MainActivity.kt
+++ app/src/main/java/com/irmabf/appofthrones/MainActivity.kt
@@ -14,14 +14,10 @@ class MainActivity : AppCompatActivity() {
 
         val button: Button = findViewById(R.id.button)
 
-        button.setOnClickListener(object : View.OnClickListener{
-            override fun onClick(v: View?) {
-                //Toast needs a context, a connection between the SO and the controllers,
-                //that context needs to reference the current class, in this case, MainActivity
-                Toast.makeText(this@MainActivity, "Winter is coming", Toast.LENGTH_SHORT).show()
-            }
-
-        })
+        button.setOnClickListener {
+            //Toast needs a context, a connection with the controllers, that context needs to reference the current class, in this case, MainActivity
+            Toast.makeText(this@MainActivity, "Winter is coming", Toast.LENGTH_SHORT).show()
+        }
 
     }
 }
