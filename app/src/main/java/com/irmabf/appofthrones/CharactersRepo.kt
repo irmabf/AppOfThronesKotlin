0bc7f31a1348830a0e011e93da9544746e591169
Irma <irblancofdz@gmail.com>
2018-09-06 13:20:20 +0200

    Refactor CharactersRepo to add getters

--- app/src/main/java/com/irmabf/appofthrones/CharactersRepo.kt
+++ app/src/main/java/com/irmabf/appofthrones/CharactersRepo.kt
@@ -6,16 +6,9 @@ object CharactersRepo {
 
     //Function which returns a list of dummy characters
     private fun dummyCharacters(): MutableList<Character> {
-        //1. Create the variable dummies as mutable list and a range
-        // as an array of integers from 1 to 10
-        //.2 We map through each element inside the range applying a lambda function which
-        //receives and index variable - the int in the range -, and returns a Character
-        val dummies: MutableList<Character> = (1..10).map { index ->
-            intToCharacter(index)
+        return (1..10).map {
+            intToCharacter(it)
         }.toMutableList()
-
-        //4. Return dummies
-        return dummies
     }
 
     private  fun intToCharacter(int: Int): Character {
